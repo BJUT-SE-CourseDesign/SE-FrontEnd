7dc4695d9e73d10a3d2982c29fa37b35aa5e164a
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-07-12 01:55:18 +0800

    可以加载文件，打开文件

--- src/components/PaperReader.vue
+++ src/components/PaperReader.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="Webviewer">
     <WebViewer
+      :loadPDF="
+        (instance) => {
+          loadPDF(instance);
+        }
+      "
       initialDoc="https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf"
     />
   </div>
@@ -17,7 +22,18 @@ export default {
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    loadPDF(instance) {
+      const tabIndex = this.$store.state.tabIndex;
+      const file = this.$store.state.fileObjs[tabIndex];
+      instance.loadDocument(file, { filename: file.name });
+      const { docViewer } = instance;
+      docViewer.on("documentLoaded", () => {
+        // perform document operations
+      });
+      console.log("pdf loaded");
+    },
+  },
 };
 </script>
 

714b8cb8e0f93e3dd33c757c05fa4bac0132efc7
Tianhe Wu <tianhe_wu@foxmail.com>
2021-07-13 21:22:33 +0800

    modify admin page

--- src/components/MemberList.vue
+++ src/components/MemberList.vue
@@ -1,36 +1,38 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column label="姓名" width="100vh">
-      <template #default="scope">
-        <i class="el-icon-user"></i>
-        <span style="margin-left: 10px">{{ scope.row.name }}</span>
-      </template>
+  <el-table :data="tableData" style="width: 50%">
+    <el-table-column label="用户名称" prop="id"> </el-table-column>
+    <el-table-column label="密码" prop="name"> </el-table-column>
+    <el-table-column label="修改密码" prop="desc">
+      <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+        ><i class="el-icon-edit"></i>
+      </el-button>
     </el-table-column>
-    <el-table-column prop="password" label="密码" width="120vh">
-    </el-table-column>
-    <el-table-column label="修改密码" width="100vh">
-      <template #default="scope">
-        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-          ><i class="el-icon-edit"></i
-        ></el-button>
-      </template>
-    </el-table-column>
-    <el-table-column label="删除用户" width="100vh">
-      <template #default="scope">
-        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-          ><i class="el-icon-delete"></i
-        ></el-button>
-      </template>
-    </el-table-column>
-    <el-table-column label="查看文件夹" width="100vh">
-      <template #default="scope">
-        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-          ><i class="el-icon-folder-opened"></i
-        ></el-button>
-      </template>
+    <el-table-column label="删除用户" prop="name"
+      ><el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+        ><i class="el-icon-delete"></i> </el-button
+    ></el-table-column>
+    <el-table-column label="查看文件夹" type="expand" width="120vh">
     </el-table-column>
   </el-table>
 </template>
+
+<style>
+/* .el-table__expand-column .cell {
+  display: none;
+} */
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
+</style>
 
 <script>
 export default {
@@ -38,28 +40,43 @@ export default {
     return {
       tableData: [
         {
-          password: "cyqsb",
-          name: "吴天鹤",
-          address: "上海市普陀区金沙江路 1518 弄",
+          id: "12987122",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
         },
         {
-          password: "I am sb",
-          name: "陈宇卿",
-          address: "上海市普陀区金沙江路 1517 弄",
+          id: "12987123",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
+        },
+        {
+          id: "12987125",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
+        },
+        {
+          id: "12987126",
+          name: "好滋好味鸡蛋仔",
+          category: "江浙小吃、小吃零食",
+          desc: "荷兰优质淡奶，奶香浓而不腻",
+          address: "上海市普陀区真北路",
+          shop: "王小虎夫妻店",
+          shopId: "10333",
         },
       ],
     };
   },
-  methods: {
-    handleEdit(index, row) {
-      console.log(index, row);
-    },
-    handleDelete(index, row) {
-      console.log(index, row);
-    },
-  },
 };
 </script>
-
-<style>
-</style>
--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,15 +1,18 @@
 <template>
   <MemberList />
+  <SystemSet />
 </template>
 
 
 <script>
 import MemberList from "../components/MemberList.vue";
+import SystemSet from "../components/SystemSet.vue";
 
 export default {
   name: "MainPage",
   components: {
     MemberList,
+    SystemSet,
   },
   data() {
     return {};
